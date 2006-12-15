7d33953f7984b022aeaa5353369949538c2ee6fb
jcomeau_ictx <jcomeau_ictx>
2006-12-15 00:12:18 +0000

    Loveall DMA code assembles but doesn't yet work. Bochs complains that floppy drive 3 was selected, and that that doesn't exist.

--- as/boot.asm
+++ as/boot.asm
@@ -123,54 +123,89 @@ protected: ;# now in protected 32-bit mode
 .endif
     xor  ecx, ecx
 
-a20: mov  al, 0x0d1
+a20:
+    mov  al, 0x0d1
     out  0x64, al ;# to keyboard
 0:  in   al, 0x64
     and  al, 2
     jnz  0b
     mov  al, 0x4b
     out  0x60, al ;# to keyboard, enable A20
-
+.ifndef DMA
     call dma ;# set up for non-dma floppy access
     shl  ebx, 4
     add  esi, ebx
     cmp  dword ptr [esi], 0x44444444 ;# boot?
     jnz  cold
+.else
+    cmp  si, 0x7c00+0x200 ;# boot?
+    jz   cold
+    shl  ebx, 4
+    add  esi, ebx
+.endif
     mov  cx, 63*0x100-0x80 ;# nope
     rep movsd
     mov  esi, offset godd ;# 0x9f448, 3000 bytes below 0xa0000 (gods)
     jmp  start2
 
-cold: call sense_
+cold:
+.ifndef DMA
+    call sense_
     jns  cold
+.endif
     mov  esi, offset godd ;# 0x9f448, 3000 bytes below 0xa0000 (gods)
+.ifdef DMA
+    call spin
+    call dma
+.endif
     xor  edi, edi ;# cylinder 0 on top of address 0
+.ifdef DMA
+    call read
+    inc dword ptr cylinder
+.endif
     mov  cl, byte ptr nc ;# number of cylinders used
+.ifdef DMA
+    dec  cl
+.endif
 0:  push ecx
     call read
-    inc dword ptr  cylinder
+.ifndef DMA
+    inc dword ptr cylinder
+.endif
     pop  ecx
     loop 0b
+.ifndef DMA
 start2: call stop
     jmp  start1
-
+.endif
 .equ us, 1000/6
 .equ ms, 1000*us
 spin: mov  cl, 0x1c
     call onoff
-;#  mov  dx, 0x3f2
-;#  out  dx, al
-0:  call sense_
+.ifdef DMA
+    mov  ecx, 400*ms
+.endif
+0:
+.ifndef DMA
+    call sense_
     jns  0b
-    mov byte ptr cylinder, 0 ;# calibrate
+    mov  byte ptr cylinder, 0 ;# calibrate
+.else
+    loop 0b
+    mov  byte ptr cylinder, cl
+.endif
     mov  al, 7
     mov  cl, 2
+.ifndef DMA
     call cmd
     mov  ecx, 500*ms
 0:  loop 0b
 cmdi: call sense_
     js   cmdi
     ret
+.else
+    jmp  cmdi
+.endif
 
 ready: ;#call delay
     mov  dx, 0x3f4
@@ -180,15 +215,19 @@ ready: ;#call delay
 .endif
     shl  al, 1
     jnc  0b
-    lea  edx, [edx+1]
+    lea  edx, [edx+1] ;# doesn't affect flags as INC would
     ret
 
+.ifndef DMA
 transfer: mov  cl, 9
-cmd: lea  edx, command
+cmd:
+    lea  edx, command
     mov  [edx], al
-cmd0: push esi
+cmd0:
+    push esi
     mov  esi, edx
-cmd1: call ready
+cmd1:
+    call ready
     jns  0f
     in   al, dx
     jmp  cmd1
@@ -200,12 +239,43 @@ cmd1: call ready
     loop cmd1
     pop  esi
     ret
-
+.else
+cmd:
+    push esi
+    lea  esi, command
+    mov  [esi], al
+0:  call ready
+    jns  cmd0
+    in   al, dx
+    jmp  0b
+cmd0:
+    lodsb
+    out  dx, al
+    push ecx
+    mov  ecx,0x1e
+cmd1:
+    loop cmd1
+    pop  ecx
+    loop 0b
+    pop  esi
+    ret
+transfer:
+    mov  cl, 9
+    call cmd
+    inc  byte ptr cylinder
+0:  call ready
+    jns  0b
+    ret
+.endif
 sense_: mov  al, 8
     mov  ecx, 1
     call cmd
+.ifndef DMA
 0:  call ready
     jns  0b
+.else
+    call ready
+.endif
     in   al, dx
 .ifdef QUESTIONABLE
     debugout
@@ -214,22 +284,47 @@ sense_: mov  al, 8
 ;#  cmp  al, 0x80
     ret
 
-seek: call sense_
-    jns  seek
+seek:
+.ifdef DMA
+    out 0xb, al
+.endif
+0:  call sense_
+    jns  0b
+.ifdef DMA
+    mov  al, 0xf
+    mov  cl, 3
+cmdi:
+    call cmd
+0:  call sense_
+    jz   0b
+.endif
     ret
 
-stop: mov  cl, 0x0c ;# motor off
-onoff: dup_
+stop:
+.ifndef DMA
+    mov  cl, 0x0c ;# motor off
+.else
+    mov  al, 0x0c
+    mov  dword ptr trash, buffer*4
+.endif
+onoff:
+.ifndef DMA
+    dup_
     mov  al, cl
+.endif
     mov  dx, 0x3f2
     out  dx, al
 .ifdef QUESTIONABLE
     debugout
 .endif
+.ifndef DMA
     drop
+.endif
     ret
 
-dma: mov  word ptr command+1, 0x3a2 ;# l2 s6 u32 ms (e 2)
+dma:
+.ifndef DMA
+    mov  word ptr command+1, 0x3a2 ;# l2 s6 u32 ms (e 2)
     mov  al, 3 ;# timing
     mov  cl, 3
     call cmd
@@ -240,14 +335,44 @@ dma: mov  word ptr command+1, 0x3a2 ;# l2 s6 u32 ms (e 2)
 ;# the following instruction clears the cylinder number among other things
     mov  dword ptr command, ecx ;# 0
     ret
-
-read: call seek
+.else
+    mov  word ptr command+1, 0x2a1 ;# 2 6 16 ms (e 2)
+    mov  al, 3 ;# timing
+    mov  cl, 3
+    call cmd
+    mov  word ptr command+1, 0
+    mov  eax, buffer*4
+    out  4, al
+    mov  al, ah
+    out  4, al
+    shr  eax, 16
+    out  0x81, al
+    mov  eax, 512*18*2-1 ;# DMA 2
+    out  5, al
+    mov  al, ah
+    out  5, al
+    mov  al, 0xb
+    out  0xf, al
+    ret
+.endif
+read:
+.ifdef DMA
+    mov  al, 0x16 ;# read DMA 2
+.endif
+    call seek
     mov  al, 0x0e6 ;# read normal data
     call transfer
 ;# about to read 0x4800, or 18432 bytes
 ;# total of 165888 (0x28800) bytes in 9 cylinders, 1.44 MB in 80 cylinders
 ;# note that the first call overwrites the cylinder number with 1 from
 ;# CM's color.com image; that's why it skips from cylinder 0 to 2
+.ifdef DMA
+    push esi
+    mov  esi, buffer*4
+    mov  ecx, 512*18*2/4
+    rep  movsd
+    pop  esi  
+.else
     mov  cx, 18*2*512 ;# two heads, 18 sectors/track, 512 bytes/sector
 0:  call ready
     in   al, dx
@@ -256,9 +381,28 @@ read: call seek
 .endif
     stosb
     next 0b
+.endif
     ret
 
-write: call seek
+;# don't need 'write' till after bootup
+.ifndef QUESTIONABLE
+.org 0x1fe ;# mark boot sector
+    .word 0x0aa55
+;# end of boot sector
+    .long 0x44444444 ;# mark color.com
+.endif
+
+write:
+.ifdef DMA
+    mov  edi, buffer*4
+    mov  ecx, 512*18*2/4
+    rep  movsd
+    mov  al, 0x1a ;# write DMA 2
+    call seek
+    mov  al, 0xc5
+    jmp  transfer
+.else
+    call seek
     mov  al, 0x0c5 ;# write data
     call transfer
     mov  cx, 18*2*512 ;#two heads, 18 sectors/track, 512 bytes/sector
@@ -270,13 +414,28 @@ write: call seek
 .endif
     next 0b
     ret
+.endif
 
+.ifdef QUESTIONABLE
 .org 0x1fe ;# mark boot sector
     .word 0x0aa55
 ;# end of boot sector
     .long 0x44444444 ;# mark color.com
+.endif
 
-flop: mov  cylinder, al ;# c-cx
+flop:
+.ifdef DMA
+    dup_
+    call spin
+    drop
+    mov  ecx, eax
+    drop
+    mov  cylinder, al
+    drop
+    shl  eax, 2
+    ret
+.else
+    mov  cylinder, al ;# c-cx
     dup_
     mov  dx, 0x3f2
     in   al, dx
@@ -287,19 +446,56 @@ flop: mov  cylinder, al ;# c-cx
     jnz  0f
     jmp  spin
 0:  ret
+.endif
 
-readf: call flop ;# ac-ac
+.ifdef DMA
+readf:
+    call flop
+    push edi
+    mov  edi, eax
+0:  push ecx
+    call read
+    pop  ecx
+    next 0b
+    pop  edi
+readf1:
+    call stop
+    drop
+    ret
+
+save_:
+    dup_
+    xor  eax, eax 
+    dup_
+    dup_
+    mov  eax, nc
+
+writef:
+    call flop
+    push esi
+    mov  esi, eax
+0:  push ecx
+    call write
+    pop  ecx
+    next 0b
+    pop  ESI
+    jmp  readf1
+.else
+readf:
+    call flop ;# ac-ac
     push edi
     mov  edi, [esi+4]
     shl  edi, 2
     call read
     pop  edi
-readf1: drop
+readf1:
+    drop
     inc  eax
     add  dword ptr [esi], 0x1200
     ret
 
-writef: call flop ;# ac-ac
+writef:
+    call flop ;# ac-ac
     push esi
     mov  esi, [esi+4]
     shl  esi, 2
@@ -307,8 +503,8 @@ writef: call flop ;# ac-ac
     pop  esi
     jmp  readf1
 
-seekf: call flop ;# c-c
-;#  call delay
+seekf:
+    call flop ;# c-c
     call seek
     mov  al, 0x0f
     mov  cl, 3
@@ -328,3 +524,4 @@ readyf: dup_
     call ready
     drop
     ret
+.endif
