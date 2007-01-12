5e82e9b16f9f5146db33091bdc7fdef82aca06f4
jcomeau_ictx <jcomeau_ictx>
2007-01-12 00:09:22 +0000

    will need to change this to assemble 32x32 character maps

---
+++ as/chr32x32.asm
@@ -0,0 +1,12 @@
+;# compile character map from data stored as "######.......###"
+;# the above should compile to the two bytes 0xfc 0x03
+.macro CHR16X24 row
+ .equ packed, 0
+ .irpc pixel, "\row"
+  .equ packed, packed << 1
+  .ifeqs "\pixel", "#"
+   .equ packed, packed | 1
+  .endif
+ .endr
+ .byte packed >> 8, packed & 0xff
+.endm
