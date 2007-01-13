418436c33e68b6a820dcbeec59cc59fca130760e
jcomeau_ictx <jcomeau_ictx>
2007-01-13 20:25:35 +0000

    combined two macro files into one

--- as/charmap.asm
+++ as/charmap.asm
@@ -1,3 +1,16 @@
+;# compile character map from data stored as "######.......###"
+;# the above should compile to the two bytes 0xfc 0x07
+.macro CHR16X24 row
+ .equ packed, 0
+ .irpc pixel, "\row"
+  .equ packed, packed << 1
+  .ifeqs "\pixel", "#"
+   .equ packed, packed | 1
+  .endif
+ .endr
+ .byte packed >> 8, packed & 0xff
+.endm
+
 ;# compile character map from data stored as "######........#######..........#"
 ;# the above should compile to the bytes 0xfc 0x03 0xf8 0x01
 .macro CHR32X32 row
