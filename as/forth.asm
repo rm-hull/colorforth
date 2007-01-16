28e17a4f3530b6fdb0ceb9baa6f44a6ee857545f
jcomeau_ictx <jcomeau_ictx>
2007-01-16 21:46:33 +0000

    still not quite right but closest yet. shows no difference in cf2text output. now shows progress while compiling high-level forth

--- as/forth.asm
+++ as/forth.asm
@@ -1,10 +1,12 @@
-.equ blockstart, .
+.equ blockstart, .; .equ blocknumber, -1
 
 ;# pad to block boundary
 .macro BLOCK number
  .ifb \number
+  .print "compiling next block"
   .equ blocknumber, blocknumber + 1
  .else
+  .print "compiling block \number"
   .equ blocknumber, \number
  .endif
  .ifdef ABSOLUTE_BLOCKNUMBER
@@ -91,18 +93,21 @@
 .endm
 
 .macro FORTHWORD word
- .equ packed, 0; .equ bitcount, 32; .equ compiled, 0; .equ overshifted, 0
+ .equ packed, 0; .equ savepacked, 0; .equ bitcount, 32; .equ compiled, 0
  .irpc letter, "\word"
-  .equ savepacked, packed; .equ huffindex, 0; .equ huffcode, 0
-  PACKCHAR "\letter"
-  .if bitcount < 0; .equ overshifted, 1; .endif
-  ;# low 4 bits cannot be occupied with packed stuff, and
-  ;# we can't shift more than once past 32 bits
-  .if (packed & 0xf > 0) || overshifted
-   .equ packed, huffcode << (32 - bitshift)
-   .long savepacked | typetag
-   .equ bitcount, 32 - bitshift; .equ overshifted, 0
-   .equ typetag, 0  ;# anything further will be an extension of this word
+  .equ huffindex, 0; .equ huffcode, 0
+  GETCODE "\letter"
+  ;#.long packed, huffcode, bitcount, bitshift ;# debugging
+  .equ savepacked, packed
+  .equ packed, packed | (huffcode << (bitcount - bitshift))
+  .equ bitcount, bitcount - bitshift
+  ;# low 4 bits reserved for word type tag
+  .if (packed & 0xf > 0) || bitcount < 0
+   .long savepacked | typetag; .equ packed, huffcode << (32 - bitshift)
+   .equ bitcount, 32 - bitshift; .equ typetag, 0  ;# word "extension"
+  .elseif bitcount < bitshift  ;# we can't shift more than once past 32 bits
+   .long packed | typetag; .equ packed, 0; .equ bitcount, 32
+   .equ typetag, 0  ;# any more letters form word "extension"
   .endif
  .endr
  .ifne packed
@@ -110,7 +115,7 @@
  .endif
 .endm
 
-.macro PACKCHAR letter
+.macro GETCODE letter
  .nolist  ;# don't pollute listing with all these byte comparisons
  .irpc huffman, " rtoeanismcylgfwdvpbhxuq0123456789j-k.z/;:!+@*,?"
   .ifeqs "\letter", "\huffman"
@@ -118,8 +123,6 @@
    .ifge bitshift - 6
     .equ bitshift, 7
    .endif
-   .equ packed, packed | (huffcode << (bitcount - bitshift))
-   .equ bitcount, bitcount - bitshift
    .exitm
   .else
    NEXTCODE
