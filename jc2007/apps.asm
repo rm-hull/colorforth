ff1b61bf580f64f580e6d23c71f699e11312ba61
jcomeau_ictx <jcomeau_ictx>
2007-03-25 22:46:25 +0000

    fixed stack bug in z**2 still appears to be skipping every 3rd pixel or so

--- jc2007/apps.asm
+++ jc2007/apps.asm
@@ -17,7 +17,7 @@ FORTH pnext, ;# xval, yval, check, drop, drop,
  FORTH [COMPILEWORD], x, 0, or, drop, if, ";", # done if x nonzero
  FORTH [COMPILEWORD], then, y, [COMPILESHORT], 1, +, vp, mod, ;# increment y
  FORTH [EXECUTE], ynow, "!", ";"
-;#FORTH [EXECUTESHORT], 66, [EXECUTE], load
+FORTH [EXECUTESHORT], 66, [EXECUTE], load
 
 BLOCK 65
 FORTH allot, grabs, space, at, [COMPILEWORD], here, and, returns, that, "address;", z, points, to, the, array, of, values, as, generated, by, "z**2+z0"
@@ -31,14 +31,14 @@ FORTH ok, sets, the, display, and, starts, the, generator
 BLOCK 66
 FORTH z**2, [TEXT], a-a, [EXECUTE], flag, 0, !, ;# clear overrun flag
  FORTH [COMPILEWORD], dup, dup, @, over, [COMPILESHORT], 1, +, @, ;# -axy
- FORTH [COMPILEWORD], over, dup, fx*, if, ;# too big to fit 3 bits
+ FORTH [COMPILEWORD], over, dup, fx*, if, ;# x**2 too big to fit 3 bits
  FORTH [EXECUTE], flag, dup, !, then, ;# anything nonzero is a "yes"
- FORTH [COMPILEWORD], over, dup, fx*, if, 
+ FORTH [COMPILEWORD], over, dup, fx*, if, ;# y**2 too big
  FORTH [EXECUTE], flag, dup, !, then,
  FORTH [COMPILEWORD], negate, +, push,
- FORTH [COMPILEWORD], fx*, if,
+ FORTH [COMPILEWORD], fx*, if, ;# x*y too big
  FORTH [EXECUTE], flag, dup, !, then, 2*,
- FORTH [COMPILEWORD], swap, [COMPILESHORT], 1, +, !, pop, swap, !, ";"
+ FORTH [COMPILEWORD], over, [COMPILESHORT], 1, +, !, pop, swap, !, ";"
 FORTH z+, [TEXT], aa-, ;# add two complex numbers, storing result in first
  FORTH [COMPILEWORD], over, [COMPILESHORT], 1, +, @, ;# -aav
  FORTH [COMPILEWORD], over, [COMPILESHORT], 1, +, @, ;# -aavv
@@ -47,18 +47,18 @@ FORTH z+, [TEXT], aa-, ;# add two complex numbers, storing result in first
 FORTH iter, ;# iterate through the array of complex numbers, updating
  ;# this does one pixel at a time
  FORTH [COMPILEWORD], xval, z0, !, yval, z0, [COMPILESHORT], 1, +, !,
- FORTH [COMPILEWORD], x, y, hp, *, +, dup, ;# locate pixel
- FORTH [COMPILESHORT], -1, +, 2*, ;# zero-base index into 'z' table
- FORTH [EXECUTE], z, @, +, z**2, z0, swap, z+, ;# z is now z**2+z0
+ FORTH [COMPILEWORD], x, y, hp, *, +, [COMPILESHORT], -1, + ;# 0-based pixel
+ FORTH [COMPILEWORD], dup, 2*, ;# doubled for z-table, single for display
+ FORTH [EXECUTE], z, @, +, z**2, z0, z+, ;# z is now z**2+z0
  FORTH [COMPILEWORD], vframe, +, [COMPILESHORT], -8, swap, +!, ;# bump color
  FORTH [COMPILEWORD], pnext, ";"
-FORTH init, /* [TEXT], -2.1, */
+FORTH init, [TEXT], -2.1,
  FORTH [EXECUTESHORT], -2100, [EXECUTE], fixed, nop, [EXECUTE], xl, "!",
- ;# FORTH [TEXT], "1.1",
+ FORTH [TEXT], "1.1",
  FORTH [EXECUTESHORT], 1100, [EXECUTE], fixed, nop, [EXECUTE], xr, "!",
- ;# FORTH [TEXT], "1.2",
+ FORTH [TEXT], "1.2",
  FORTH [EXECUTESHORT], 1200, [EXECUTE], fixed, nop, [EXECUTE], yt, "!",
- ;# FORTH [TEXT], "-1.2",
+ FORTH [TEXT], "-1.2",
  FORTH [EXECUTESHORT], -1200, [EXECUTE], fixed, nop, [EXECUTE], yb, "!",
  FORTH [EXECUTE], xr, "@", [EXECUTE], xl, "@", negate, "+",
  FORTH [EXECUTE], xspan, "!",
