cdacd91a7b976550ce74bdcab48ef79b1c542093
jcomeau_ictx <jcomeau_ictx>
2007-03-26 06:21:47 +0000

    got rid of code-overwriting bug but still no outline of Mandelbrot set

--- jc2007/apps.asm
+++ jc2007/apps.asm
@@ -1,20 +1,23 @@
 BLOCK 64
-FORTH [TEXTCAPITALIZED], mandelbrot, [TEXT], display, [EXECUTE], empty, [EXECUTE], forth, [VARIABLE], xl, [BINARY], 0, [VARIABLE], xr, [BINARY], 0, [VARIABLE], yt, [BINARY], 0, [VARIABLE], yb, [BINARY], 0, [VARIABLE], xspan, [BINARY], 0, [VARIABLE], yspan, [BINARY], 0, [VARIABLE], xnow, [BINARY], 0, [VARIABLE], ynow, [BINARY], 0, [VARIABLE], flag, [BINARY], 0
-FORTH allot, [TEXT], n-a, here, swap, for, 0, ",", next, ";", [VARIABLE], z, [BINARY], 0, [EXECUTE], hp, [EXECUTE], vp, [EXECUTE], "*", [EXECUTE], [EXECUTESHORT], 1, [EXECUTE], +, [EXECUTE], dup, [EXECUTE], "+", [EXECUTE], allot, [EXECUTE], z, [EXECUTE], "!" 
+FORTH [TEXTCAPITALIZED], mandelbrot, [TEXT], display, [EXECUTE], empty, [EXECUTE], forth, [VARIABLE], xl, [BINARY], 0, [VARIABLE], xr, [BINARY], 0, [VARIABLE], yt, [BINARY], 0, [VARIABLE], yb, [BINARY], 0, [VARIABLE], xspan, [BINARY], 0, [VARIABLE], yspan, [BINARY], 0, [VARIABLE], xnow, [BINARY], 0, [VARIABLE], ynow, [BINARY], 0, [VARIABLE], flag, [BINARY], 0, [VARIABLE], dark, [BINARY], 0, [VARIABLE], pause, [BINARY], 0
+FORTH allot, [TEXT], n-a, here, [COMPILESHORT], 3, +, [COMPILESHORT], 4, /, swap, for, 0, ",", next, ";", [VARIABLE], z, [BINARY], 0, [EXECUTE], hp, [EXECUTE], vp, [EXECUTE], "*", [EXECUTE], [EXECUTESHORT], 1, [EXECUTE], +, [EXECUTE], dup, [EXECUTE], "+", [EXECUTE], allot, [EXECUTE], z, [EXECUTE], "!" 
 FORTH fixed, [EXECUTELONGHEX], 10000000, [EXECUTESHORT], 1000, [EXECUTE], /, *, ";"
 FORTH x, [EXECUTE], xnow, @, ";"
 FORTH y, [EXECUTE], ynow, @, ";"
-FORTH z0, [TEXT], -a, [EXECUTE], z, @, [EXECUTE], vp, [EXECUTE], hp, [EXECUTE], *, +, ";"
+FORTH z0, [TEXT], -a, [EXECUTE], z, @, vp, hp, *, dup, +, +, ";"
 FORTH xval, x, [COMPILELONGHEX], 10000000, hp, "*/", ;# scale to A(3,28) fixed
  FORTH [EXECUTE], xspan, @, fx*, [EXECUTE], xl, @, +, ";"
 FORTH yval, y, [COMPILELONGHEX], 10000000, vp, "*/", ;# make fixed-point number
  FORTH [EXECUTE], yspan, @, fx*, negate, [EXECUTE], yt, @, +, ";"
 FORTH check, over, over, ;# leave two items on the stack
  FORTH [COMPILESHORT], 3, [COMPILESHORT], 200, at, h., space, h., ";"
+FORTH xnext, x, [COMPILESHORT], 64, +, hp, mod, [EXECUTE], xnow, !, ";"
+FORTH p, [EXECUTE], pause, dup, @,
+ FORTH [COMPILESHORT], 1, or, swap, !, ";" ;# toggle
 FORTH pnext, ;# xval, yval, check, drop, drop,
- FORTH [COMPILEWORD], x, [COMPILESHORT], 1, +, hp, mod,
- FORTH [EXECUTE], xnow, "!", ;# increment x
- FORTH [COMPILEWORD], x, 0, or, drop, if, ";", # done if x nonzero
+ FORTH [COMPILEWORD], x, y, check, drop, drop,
+ FORTH [COMPILEWORD], xnext,
+ FORTH [COMPILEWORD], x, 0, +, drop, if, ";", # done if x nonzero
  FORTH [COMPILEWORD], then, y, [COMPILESHORT], 1, +, vp, mod, ;# increment y
  FORTH [EXECUTE], ynow, "!", ";"
 FORTH [EXECUTESHORT], 66, [EXECUTE], load
@@ -46,12 +49,16 @@ FORTH z+, [TEXT], aa-, ;# add two complex numbers, storing result in first
  FORTH [COMPILEWORD], @, over, @, +, swap, !, ";"
 FORTH iter, ;# iterate through the array of complex numbers, updating
  ;# this does one pixel at a time
+ FORTH [EXECUTE], pause, @, 0, +, drop, if, ";", then,
+ FORTH [COMPILESHORT], -8, [EXECUTE], dark, !,
  FORTH [COMPILEWORD], xval, z0, !, yval, z0, [COMPILESHORT], 1, +, !,
  FORTH [COMPILEWORD], x, y, hp, *, +, [COMPILESHORT], -1, + ;# 0-based pixel
  FORTH [COMPILEWORD], dup, 2*, ;# doubled for z-table, single for display
- FORTH [EXECUTE], z, @, +, z**2, z0, z+, ;# z is now z**2+z0
- FORTH [COMPILEWORD], vframe, +, [COMPILESHORT], -8, swap, +!, ;# bump color
- FORTH [COMPILEWORD], pnext, ";"
+ FORTH [COMPILEWORD], pnext,
+ FORTH [EXECUTE], z, @, +, z**2,
+ FORTH [EXECUTE], flag, @, 0, +, drop, if, 0, [EXECUTE], dark, !,
+ FORTH [COMPILEWORD], then, z0, z+, ;# z is now z**2+z0
+ FORTH [COMPILEWORD], vframe, +, [EXECUTE], dark, @, swap, +!, ";"
 FORTH init, [TEXT], -2.1,
  FORTH [EXECUTESHORT], -2100, [EXECUTE], fixed, nop, [EXECUTE], xl, "!",
  FORTH [TEXT], "1.1",
@@ -63,6 +70,8 @@ FORTH init, [TEXT], -2.1,
  FORTH [EXECUTE], xr, "@", [EXECUTE], xl, "@", negate, "+",
  FORTH [EXECUTE], xspan, "!",
  FORTH [EXECUTE], yt, "@", [EXECUTE], yb, "@", negate, "+",
- FORTH [EXECUTE], yspan, "!", ";"
+ FORTH [EXECUTE], yspan, "!",
+ FORTH [COMPILEWORD], ";"
+;# put "show" before "blue, screen" for debugging; after for raster graphics
 FORTH ok, init, blue, screen, show, keyboard, debug, iter, ";"
 BLOCK
