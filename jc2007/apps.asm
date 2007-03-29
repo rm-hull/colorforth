e459c476cbd38037fd4bbd9c2251480938c66025
jcomeau_ictx <jcomeau_ictx>
2007-03-29 17:34:51 +0000

    color.com loads as far as 94b1 (in 'cold') on both freedos-img and booting from floppy still hacking mandelbrot routines

--- jc2007/apps.asm
+++ jc2007/apps.asm
@@ -21,11 +21,11 @@ FORTH yval, y, [COMPILELONGHEX], 10000000, vp, "*/", ;# make fixed-point number
  FORTH [EXECUTE], yspan, @, fx*, negate, [EXECUTE], yt, @, +, ";"
 FORTH check, over, over, ;# leave two items on the stack
  FORTH [COMPILESHORT], 3, [COMPILESHORT], 200, at, h., space, h., ";"
-FORTH xnext, x, [COMPILESHORT], 100, +, hp, mod, [EXECUTE], xnow, !, ";"
+FORTH xnext, x, [COMPILESHORT], 200, +, hp, mod, [EXECUTE], xnow, !, ";"
 FORTH p, [EXECUTE], pause, dup, @,
  FORTH [COMPILESHORT], 1, or, swap, !, ";" ;# toggle
-FORTH pnext, ;# xval, yval, check, drop, drop,
- FORTH [COMPILEWORD], x, y, check, drop, drop,
+FORTH pnext,  ;#xval, yval, check, drop, drop,
+ ;#FORTH [COMPILEWORD], x, y, check, drop, drop,
  FORTH [COMPILEWORD], xnext,
  FORTH [COMPILEWORD], x, 0, +, drop, if, ";", # done if x nonzero
  FORTH [COMPILEWORD], then, y, [COMPILESHORT], 1, +, vp, mod, ;# increment y
